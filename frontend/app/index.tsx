bacf61a9ab9bf9b9416227867a5aba4fd23c79aa
Christopher Le <79244108+oSirhcel@users.noreply.github.com>
2024-08-29 13:26:27 +1000

    chore: init project folders

---
+++ frontend/app/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/button';
+import { View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Button label='Hello, World!' />
+    </View>
+  );
+}
