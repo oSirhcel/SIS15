4ada22490ce2af8ef9ac80f955f196ba7548197d
Christopher Le <79244108+oSirhcel@users.noreply.github.com>
2024-08-29 15:06:51 +1000

    feat: add React Native Reusables

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -4,14 +4,8 @@ const { hairlineWidth } = require('nativewind/theme');
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
+  darkMode: 'class',
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
-    },
     extend: {
       colors: {
         border: 'hsl(var(--border))',
