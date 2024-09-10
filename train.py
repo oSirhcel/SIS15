8f5386037c72bfe5e33279967508d7f0ada0c5f3
wangk <wangk@qq.com>
2024-09-10 17:23:03 +0800

    path change

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import onnx
 model = YOLO('yolov8n.yaml')
 model = YOLO('yolov8n.pt')
-path = '/content/datasets/waste-detection-9/data.yaml'
+path = '/content/drive/MyDrive/Colab Notebooks/Waste/data.yaml'
 results = model.train(data=path, epochs=50)
 results = model.val()
 success = model.export(format='onnx')
