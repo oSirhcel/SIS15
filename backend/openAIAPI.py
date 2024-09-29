5de39b448a8a987f16eee842f97f7da922395864
Ethan Burgess <etho.burgess@gmail.com>
2024-09-29 13:37:42 +1000

    Open ai suggestions and update UI for all waste types

---
+++ backend/openAIAPI.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+import json
+client = OpenAI(api_key="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")
+
+
+def open_ai_response(waste_classification):
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": "Give me suggestions to recycle or reuse this waste: {}, also give me 2 companies/businesses in australia that could help in this process aswell as a link to their website or contact information return the answer in true json form e.g recycle_suggestion: xyz, reuse_suggestion: xyz, companies: [x,y]. all one line".format(waste_classification)
+            }
+        ]
+    )
+    cleaned_string = completion.choices[0].message.content.strip("`").replace("json", "")
+    return json.loads(cleaned_string)
+
+
+def format_companies(suggestions):
+    #print(suggestions)
+    for company in suggestions["companies"]:
+        print(company)
+
+#format_companies(open_ai_response("general waste"))
