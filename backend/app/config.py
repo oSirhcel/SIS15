7b682278a30681a78ad45689debb5a5244bb859e
Cxmrykk <contact@merrick.cam>
2024-10-15 15:43:11 +1100

    Remove DATABASE_URL from config

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,6 @@ load_dotenv()
 
 class Config:
     OPENAI_KEY = os.getenv('OPENAI_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     IMAGES_PATH = os.getenv('IMAGES_PATH')
     MODEL_PATH = os.getenv('MODEL_PATH')
