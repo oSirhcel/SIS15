3fdc30cc6276d85e04d1090bda53e20342218201
Cxmrykk <contact@merrick.cam>
2024-10-28 11:44:38 +1100

    Fix deprecated warning; use contentFit

--- frontend/app/(tabs)/scan.tsx
+++ frontend/app/(tabs)/scan.tsx
@@ -312,11 +312,11 @@ export default function ScanTab() {
         <View style={{ flex: 1 }}>
           <Image
             source={{ uri: currentPhoto }}
+            contentFit="cover"
             style={{
               flex: 1,
               width: '100%',
               height: '100%',
-              resizeMode: 'cover',
             }}
           />
           <View className='absolute bottom-0 left-0 right-0 h-32 bg-background'>
